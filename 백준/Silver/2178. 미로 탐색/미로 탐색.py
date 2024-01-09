7ec55c7cc3b37a5d4f15bc19c37c2196c2a4eb97
myikn <109326214+myikn@users.noreply.github.com>
2024-01-09 16:46:35 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,22 +1,9 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-graph = []
-
-for _ in range(n):
-    graph.append(list(map(int, input().rstrip()))) # readline의 경우 맨 뒤에 '\n'까지 입력받으므로 제거해줘야 함
-
-# 상하좌우
-dx = [-1, 1, 0, 0] 
-dy = [0, 0, -1, 1]
-
 def bfs(x, y):
-    
     queue = deque()
-    queue.append((x,y))
+    queue.append((x, y))
 
     while queue:
         x, y = queue.popleft()
@@ -28,7 +15,18 @@ def bfs(x, y):
             if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
                 queue.append((nx, ny))
                 graph[nx][ny] = graph[x][y] + 1
-    
-    return graph[n-1][m-1]
 
-print(bfs(0,0))
+    return graph[n - 1][m - 1]
+
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for _ in range(n):
+    graph.append(list(map(int, input().rstrip())))
+
+print(bfs(0, 0))
