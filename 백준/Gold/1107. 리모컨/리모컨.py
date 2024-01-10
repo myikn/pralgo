3fece5fa07bf51de836520751c2419b04c6ac3fd
myikn <109326214+myikn@users.noreply.github.com>
2024-01-10 11:12:22 +0900

    [Gold V] Title: 리모컨, Time: 252 ms, Memory: 110608 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,3 +1,5 @@
+import sys
+
 N = int(input())
 M = int(input())
 
@@ -7,11 +9,13 @@ else:
     num_list = []
 
 result = abs(100 - N)
+
+# 채널이 무한대이므로 올라가는것 내려가는것 모두 고려
 for i in range(1000001):
     for j in str(i):
         if j in num_list:
             break
     else:
-        result = min(result, len(str(i)) + abs(i - N)) #min(기존답, 숫자 버튼 클릭 수 + '+/-' 버튼 클릭 수)
+        result = min(result, len(str(i)) + abs(i - N))
 
 print(result)
